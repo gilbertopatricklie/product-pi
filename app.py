8ceeb58ef9c6cc176bb47d38fa3c6647a30805e2
Gilberto Patrick Lie <gilbertopatricklie@gmail.com>
2025-07-22 14:32:07 +0700

    add ip in run

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def upload():
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
     os.makedirs('static', exist_ok=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
